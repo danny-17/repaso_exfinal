fb9735416ff4e6514f66b91e62d5a97e69cee431
Danny Tenemea <danny.tenemea.est@tecazuay.edu.ec>
2022-10-05 22:16:20 -0500

    Initial commit

---
+++ src/main/java/com/example/repaso_exfinal/RepasoExfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.repaso_exfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoExfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoExfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repaso_exfinal/RepasoExfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repaso_exfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoExfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
